c45df8f04e4d15ecc310254a062166cb14ec3459
Ruben Bernardes <ruben@redcarrots.be>
2019-04-08 14:21:49 +0200

    make it work

--- app.js
+++ app.js
@@ -48,11 +48,11 @@ if (cluster.isMaster) {
 
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
-    // app.use(bodyParser.urlencoded({extended:false}));
+    app.use(bodyParser.urlencoded({extended:false}));
 
     // Middleware
-    app.use(bodyParser.json({limit: '50mb', extended: true}))
-    app.use(bodyParser.urlencoded({limit: '50mb', extended: true})) // allow images
+    // app.use(bodyParser.json({limit: '50mb', extended: true}))
+    // app.use(bodyParser.urlencoded({limit: '50mb', extended: true})) // allow images
     app.use(cors());
 
     app.use(session({
