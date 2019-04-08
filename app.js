43e3ed1f8d42cf5acac2b090a9bd16f84db95db8
Ruben Bernardes <ruben@redcarrots.be>
2019-04-08 14:06:02 +0200

    chore(): uncommented app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Include the cluster module
-const cluster = require('cluster');
+var cluster = require('cluster');
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
 
     // Count the machine's CPUs
-    const cpuCount = require('os').cpus().length;
+    var cpuCount = require('os').cpus().length;
 
     // Create a worker for each CPU
-    for (let i = 0; i < cpuCount; i += 1) {
+    for (var i = 0; i < cpuCount; i += 1) {
         cluster.fork();
     }
 
@@ -23,48 +23,18 @@ if (cluster.isMaster) {
 
 // Code to run if we're in a worker process
 } else {
-    const AWS = require('aws-sdk');
-    const express = require('express');
-    const session = require('express-session');
-    const bodyParser = require('body-parser');
-    const cors = require('cors');
-    const dotenv = require('dotenv');
-    const salesforce = require('./config/salesforce');
-    dotenv.config();
+    var AWS = require('aws-sdk');
+    var express = require('express');
+    var bodyParser = require('body-parser');
 
     AWS.config.region = process.env.REGION
 
-    const sns = new AWS.SNS();
-    const ddb = new AWS.DynamoDB();
+    var sns = new AWS.SNS();
+    var ddb = new AWS.DynamoDB();
 
-    // Load Routes
-    // const contracts = require('./routes/contracts');
-    // const query = require('./routes/query');
-    // const update = require('./routes/update');
-    // const emailservice = require('./routes/emailservice');
-
-    const ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
-    const snsTopic =  process.env.NEW_SIGNUP_TOPIC;
-    const app = express();
-
-    // app.use(session({
-    //   secret: 's3cret', // it can be anything we want
-    //   resave: true, // changed to true
-    //   saveUninitialized: true,
-    //   org: {}, // salesforce
-    //   token: null // salesforce
-    // }));
-
-    // // Middleware
-    // app.use(bodyParser.json({limit: '50mb', extended: true}))
-    // app.use(bodyParser.urlencoded({limit: '50mb', extended: true})) // allow images
-    // app.use(cors());
-
-    // // Routes
-    // app.use('/contracts', contracts);
-    // app.use('/query', query);
-    // app.use('/update', update);
-    // app.use('/emailservice', emailservice);
+    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+    var app = express();
 
     app.set('view engine', 'ejs');
     app.set('views', __dirname + '/views');
@@ -78,10 +48,8 @@ if (cluster.isMaster) {
         });
     });
 
-
-
     app.post('/signup', function(req, res) {
-        const item = {
+        var item = {
             'email': {'S': req.body.email},
             'name': {'S': req.body.name},
             'preview': {'S': req.body.previewAccess},
@@ -94,7 +62,7 @@ if (cluster.isMaster) {
             'Expected': { email: { Exists: false } }
         }, function(err, data) {
             if (err) {
-                const returnStatus = 500;
+                var returnStatus = 500;
 
                 if (err.code === 'ConditionalCheckFailedException') {
                     returnStatus = 409;
@@ -121,9 +89,9 @@ if (cluster.isMaster) {
         });
     });
 
-    const port = process.env.PORT || 3000;
+    var port = process.env.PORT || 3000;
 
-    const server = app.listen(port, function () {
+    var server = app.listen(port, function () {
         console.log('Server running at http://127.0.0.1:' + port + '/');
     });
 }
