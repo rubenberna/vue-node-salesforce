e42ddfdf77b7e2bbf443656b21d5e67bc7f34f57
Ruben Bernardes <ruben@redcarrots.be>
2019-04-08 14:52:05 +0200

    make it work

--- app.js
+++ app.js
@@ -77,58 +77,78 @@ if (cluster.isMaster) {
     app.use('/update', update);
     app.use('/emailservice', emailservice);
 
-    app.get('/', function(req, res) {
-        res.render('index', {
-            static_path: 'static',
-            theme: process.env.THEME || 'flatly',
-            flask_debug: process.env.FLASK_DEBUG || 'false'
-        });
+    // Handle production
+    if(process.env.NODE_ENV === 'production') {
+      // Static folder
+      app.use(express.static(__dirname + '/public/' ))
+      // Handle SPA
+      app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
+      // reads: any route at all, send the file index.html located in the public folder
+      // Use .env variables
+      require('dotenv').load();
+    }
+
+    // app.get('/', function(req, res) {
+    //     res.render('index', {
+    //         static_path: 'static',
+    //         theme: process.env.THEME || 'flatly',
+    //         flask_debug: process.env.FLASK_DEBUG || 'false'
+    //     });
+    // });
+
+    // app.post('/signup', function(req, res) {
+    //     const item = {
+    //         'email': {'S': req.body.email},
+    //         'name': {'S': req.body.name},
+    //         'preview': {'S': req.body.previewAccess},
+    //         'theme': {'S': req.body.theme}
+    //     };
+    //
+    //     ddb.putItem({
+    //         'TableName': ddbTable,
+    //         'Item': item,
+    //         'Expected': { email: { Exists: false } }
+    //     }, function(err, data) {
+    //         if (err) {
+    //             const returnStatus = 500;
+    //
+    //             if (err.code === 'ConditionalCheckFailedException') {
+    //                 returnStatus = 409;
+    //             }
+    //
+    //             res.status(returnStatus).end();
+    //             console.log('DDB Error: ' + err);
+    //         } else {
+    //             sns.publish({
+    //                 'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email
+    //                                     + "\r\nPreviewAccess: " + req.body.previewAccess
+    //                                     + "\r\nTheme: " + req.body.theme,
+    //                 'Subject': 'New user sign up!!!',
+    //                 'TopicArn': snsTopic
+    //             }, function(err, data) {
+    //                 if (err) {
+    //                     res.status(500).end();
+    //                     console.log('SNS Error: ' + err);
+    //                 } else {
+    //                     res.status(201).end();
+    //                 }
+    //             });
+    //         }
+    //     });
+    // });
+
+    // Start server
+    const port = process.env.PORT || 5000;
+
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+      salesforce.login();
     });
 
-    app.post('/signup', function(req, res) {
-        const item = {
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
 
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }
-        }, function(err, data) {
-            if (err) {
-                const returnStatus = 500;
-
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
-
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });
-            }
-        });
-    });
-
-    const port = process.env.PORT || 3000;
-
-    const server = app.listen(port, function () {
-        console.log('Server running at http://127.0.0.1:' + port + '/');
-    });
+    // const port = process.env.PORT || 3000;
+    //
+    // const server = app.listen(port, function () {
+    //     console.log('Server running at http://127.0.0.1:' + port + '/');
+    // });
 }
