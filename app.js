a127f4803627f643d5a517db6d70538a74e9d799
Ruben Bernardes <ruben@redcarrots.be>
2019-04-08 13:53:35 +0200

    chore(): commented app.js

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Include the cluster module
-var cluster = require('cluster');
+const cluster = require('cluster');
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
 
     // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+    const cpuCount = require('os').cpus().length;
 
     // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
+    for (let i = 0; i < cpuCount; i += 1) {
         cluster.fork();
     }
 
@@ -23,22 +23,52 @@ if (cluster.isMaster) {
 
 // Code to run if we're in a worker process
 } else {
-    var AWS = require('aws-sdk');
-    var express = require('express');
-    var bodyParser = require('body-parser');
+    const AWS = require('aws-sdk');
+    const express = require('express');
+    const session = require('express-session');
+    const bodyParser = require('body-parser');
+    const cors = require('cors');
+    const dotenv = require('dotenv');
+    const salesforce = require('./config/salesforce');
+    dotenv.config();
 
     AWS.config.region = process.env.REGION
 
-    var sns = new AWS.SNS();
-    var ddb = new AWS.DynamoDB();
+    const sns = new AWS.SNS();
+    const ddb = new AWS.DynamoDB();
 
-    var ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
-    var snsTopic =  process.env.NEW_SIGNUP_TOPIC;
-    var app = express();
+    // Load Routes
+    const contracts = require('./routes/contracts');
+    const query = require('./routes/query');
+    const update = require('./routes/update');
+    const emailservice = require('./routes/emailservice');
 
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
-    app.use(bodyParser.urlencoded({extended:false}));
+    const ddbTable =  process.env.STARTUP_SIGNUP_TABLE;
+    const snsTopic =  process.env.NEW_SIGNUP_TOPIC;
+    const app = express();
+
+    app.use(session({
+      secret: 's3cret', // it can be anything we want
+      resave: true, // changed to true
+      saveUninitialized: true,
+      org: {}, // salesforce
+      token: null // salesforce
+    }));
+
+    // Middleware
+    app.use(bodyParser.json({limit: '50mb', extended: true}))
+    app.use(bodyParser.urlencoded({limit: '50mb', extended: true})) // allow images
+    app.use(cors());
+
+    // Routes
+    app.use('/contracts', contracts);
+    app.use('/query', query);
+    app.use('/update', update);
+    app.use('/emailservice', emailservice);
+
+    // app.set('view engine', 'ejs');
+    // app.set('views', __dirname + '/views');
+    // app.use(bodyParser.urlencoded({extended:false}));
 
     app.get('/', function(req, res) {
         res.render('index', {
@@ -48,50 +78,52 @@ if (cluster.isMaster) {
         });
     });
 
-    app.post('/signup', function(req, res) {
-        var item = {
-            'email': {'S': req.body.email},
-            'name': {'S': req.body.name},
-            'preview': {'S': req.body.previewAccess},
-            'theme': {'S': req.body.theme}
-        };
+    
 
-        ddb.putItem({
-            'TableName': ddbTable,
-            'Item': item,
-            'Expected': { email: { Exists: false } }        
-        }, function(err, data) {
-            if (err) {
-                var returnStatus = 500;
+    // app.post('/signup', function(req, res) {
+    //     const item = {
+    //         'email': {'S': req.body.email},
+    //         'name': {'S': req.body.name},
+    //         'preview': {'S': req.body.previewAccess},
+    //         'theme': {'S': req.body.theme}
+    //     };
 
-                if (err.code === 'ConditionalCheckFailedException') {
-                    returnStatus = 409;
-                }
-
-                res.status(returnStatus).end();
-                console.log('DDB Error: ' + err);
-            } else {
-                sns.publish({
-                    'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email 
-                                        + "\r\nPreviewAccess: " + req.body.previewAccess 
-                                        + "\r\nTheme: " + req.body.theme,
-                    'Subject': 'New user sign up!!!',
-                    'TopicArn': snsTopic
-                }, function(err, data) {
-                    if (err) {
-                        res.status(500).end();
-                        console.log('SNS Error: ' + err);
-                    } else {
-                        res.status(201).end();
-                    }
-                });            
-            }
-        });
+        // ddb.putItem({
+        //     'TableName': ddbTable,
+        //     'Item': item,
+        //     'Expected': { email: { Exists: false } }
+        // }, function(err, data) {
+        //     if (err) {
+        //         const returnStatus = 500;
+        //
+        //         if (err.code === 'ConditionalCheckFailedException') {
+        //             returnStatus = 409;
+        //         }
+        //
+        //         res.status(returnStatus).end();
+        //         console.log('DDB Error: ' + err);
+        //     } else {
+        //         sns.publish({
+        //             'Message': 'Name: ' + req.body.name + "\r\nEmail: " + req.body.email
+        //                                 + "\r\nPreviewAccess: " + req.body.previewAccess
+        //                                 + "\r\nTheme: " + req.body.theme,
+        //             'Subject': 'New user sign up!!!',
+        //             'TopicArn': snsTopic
+        //         }, function(err, data) {
+        //             if (err) {
+        //                 res.status(500).end();
+        //                 console.log('SNS Error: ' + err);
+        //             } else {
+        //                 res.status(201).end();
+        //             }
+        //         });
+        //     }
+        // });
     });
 
-    var port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3000;
 
-    var server = app.listen(port, function () {
+    const server = app.listen(port, function () {
         console.log('Server running at http://127.0.0.1:' + port + '/');
     });
 }
