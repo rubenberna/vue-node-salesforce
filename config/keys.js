57644b7cc8dfd51950a5308805eae0967311813b
Ruben Bernardes <ruben@redcarrots.be>
2019-04-12 22:54:43 +0200

    chore(): new aws-sdk

--- config/keys.js
+++ config/keys.js
@@ -8,4 +8,7 @@ module.exports = {
   salesforceClientSecret: process.env.SF_CLIENT_SECRET,
   salesforceSecurityToken: process.env.SF_SECURITY_TOKEN,
   salesforceRedirectUri: process.env.SF_REDIRECT_URI,
+  awsRegion: process.env.AWS_REGION,
+  awsKeyId: process.env.AWS_ACCESS_KEY_ID,
+  awsKeySecret: process.env.AWS_SECRET_ACCESS_KEY
 }
