14bb6d3e7a41e2c1c9542ede771ac1e849de37a3
Ruben Bernardes <ruben@redcarrots.be>
2019-04-17 11:07:00 +0200

    feat(): removed email service

--- routes/emailservice.js
+++ routes/emailservice.js
@@ -4,22 +4,13 @@ const nodemailer = require('../config/nodemailer');
 
 router.post('/', async (req, res) => {
   const { payload } = req.body
-   try {
-     const mailing = await nodemailer.sendEmail(payload);
-     res.status(201).send('Email sent with attachment')
-   } catch (e) {
-     res.status(400).send(e)
-   }
-  
 
-  // const { payload } = req.body
-
-  // try {
-  //   const mailing = await nodemailer.sendEmail(payload);
-  //   res.status(201).send('Email sent with attachment')
-  // } catch (e) {
-  //   res.status(400).send(e)
-  // }
+  try {
+    const mailing = await nodemailer.sendEmail(payload);
+    res.status(201).send('Email sent with attachment')
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
 
 module.exports = router;
