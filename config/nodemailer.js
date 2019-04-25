e52aed1f306ce47869720b54b2f1f2892f45e7e7
Ruben Bernardes <ruben@redcarrots.be>
2019-04-25 10:18:40 +0200

    style(): changed text in confirmation email

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -21,7 +21,7 @@ module.exports = {
       from: '"EasyLife" <Customercare@easylifedc.be>', // sender address
       to: email, // list of receivers
       subject: "Easy Life - Een kopie van uw contract", // Subject line
-      html: `<p>Beste klant,</p> <p>Wij hebben uw getekende overeenkomst goed ontvangen.</p> <p>Waarvoor onze welgemeende dank!</p><p>Uw getekende overeenkomst blijft steeds beschikbaar op https://sign.easylifedc.be voor inzage door opnieuw uw code <b>${id}</b> in te geven en op de button ‘zie contract’ te klikken.<p>Voor verdere vragen of inlichtingen kan u steeds terecht bij onze Klantendienst: 0800 62 080 of customercare@easylifedc.be.</p> <p>Met vriendelijke groeten, </p> <p>Customer Care Department</p><p>Easy Life</p><p>Prins Boudewijnlaan 133</p><p>03/369 89 03</p>`, // html body
+      html: `<p>Beste klant,</p> <p>Wij hebben uw getekende overeenkomst goed ontvangen.</p> <p>Waarvoor onze welgemeende dank!</p><p>Uw getekende overeenkomst blijft steeds beschikbaar voor inzage op https://sign.easylifedc.be. Wanneer u opnieuw uw code <b>${id}</b> ingeeft en op de button ‘zie contract’ klikt, opent uw overeenkomst.</p><p>Voor verdere vragen of inlichtingen kan u steeds terecht bij onze Klantendienst: 0800 62 080 of customercare@easylifedc.be.</p> <p>Met vriendelijke groeten, </p> <p>Customer Care Department</p><p>Easy Life</p><p>Prins Boudewijnlaan 133</p><p>03/369 89 03</p>`, // html body
 
       // attachments: [
       //   {
